9641406104e2c16f75564e1c0a1df3cbab687d2d
Matthew Carroll <Matt1@Matt-Carroll.local>
2014-05-10 06:50:25 -0400

    Initial Commit

---
+++ Tile Tap Final/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tile Tap Final
+//
+//  Created by Matthew Carroll on 5/10/14.
+//  Copyright (c) 2014 Matthew Carroll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tile Tap Final/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tile Tap Final
+//
+//  Created by Matthew Carroll on 5/10/14.
+//  Copyright (c) 2014 Matthew Carroll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
